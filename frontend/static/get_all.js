209968bb3100a08fc1ec31183b05d23579b7a4f0
Maya653 <122810922+Maya653@users.noreply.github.com>
2023-11-04 23:49:38 +0000

    regresion

--- frontend/static/get_all.js
+++ frontend/static/get_all.js
@@ -1,28 +1,36 @@
-function getAll() {
-    var request = new XMLHttpRequest();
-    request.open('GET', "http://localhost:8000/contactos");
-    request.send();
+function getAll(){
+	var request = new XMLHttpRequest;
+	request.open('GET',"http://localhost:8000/contactos");
+	request.send();
 
-    request.onload = function() {
-        const response = request.responseText;
-        const json = JSON.parse(response);
-        console.log("response", json);
-        console.log("status_code:", request.status);
+	request.onload = (e) => {
+		const response = request.responseText;
+		const json = JSON.parse(response);
+		console.log("response: " + response);
+        console.log("json: " + json);
+        console.log("status_code: " + request.status);
 
-        const tbody_contactos = document.getElementById("tbody_contactos");
-        var tr = document.createElement("tr");
-        var td_email = document.createElement("td");
-        var td_nombre = document.createElement("td");
-        var td_telefono = document.createElement("td");
+        console.log("Email: " + json[0]["email"]);
+        console.log("Nombre: " + json[0]["nombre"]);
+        console.log("Telefono: " + json[0]["telefono"]);
 
-        td_email.textContent = json[0]["email"];
-        td_nombre.textContent = json[0]["nombre"];
-        td_telefono.textContent = json[0]["telefono"];
+        const tbody_contactos = document.getElementById("tybody_contactos");
 
-        tr.appendChild(td_email);
-        tr.appendChild(td_nombre);
-        tr.appendChild(td_telefono);
+        var tr = document.createElement("tr")
+		var td_email = document.createElement("td");
+		var td_nombre = document.createElement("td");
+		var td_telefono = document.createElement("td")
+	
+		td_email.innerHTML = json[0]["email"];
+		td_nombre.innerHTML = json[0]["nombre"];
+		td_telefono.innerHTML = json[0]["telefono"];
+		
+		
+		tr.appendChild(td_email);
+		tr.appendChild(td_nombre);
+		tr.appendChild(td_telefono);
 
-        tbody_contactos.appendChild(tr);
-    };
-}
+		tbody_contactos.appendChild(tr);
+				
+	};
+};
