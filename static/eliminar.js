0183b44b53899c91da3e723fc2b2a876de7a7eda
Maya653 <122810922+Maya653@users.noreply.github.com>
2023-12-02 23:06:57 +0000

    update

--- static/eliminar.js
+++ static/eliminar.js
@@ -1,14 +1,30 @@
 async function eliminarContacto() {
     try {
-        const email = prompt("Ingrese el email del contacto a eliminar:");
+        const email = document.getElementById('eliminarEmail').value;
 
         const response = await fetch(`http://localhost:8000/contactos/${email}`, {
             method: "DELETE",
         });
 
-        const data = await response.json();
-        console.log(data);
+        if (response.status === 204) {
+            // Éxito al eliminar el contacto
+            mostrarMensaje('Contacto eliminado exitosamente.');
+        } else {
+            // Error al intentar eliminar el contacto
+            mostrarMensaje('Error al intentar eliminar el contacto.');
+        }
     } catch (error) {
         console.error('Error:', error);
     }
+}
+
+function mostrarMensaje(mensaje) {
+    // Mostrar el mensaje en un div con el id "mensajeEliminacion"
+    const mensajeDiv = document.getElementById('mensajeEliminacion');
+    mensajeDiv.textContent = mensaje;
+
+    // Limpiar el mensaje después de unos segundos (opcional)
+    setTimeout(() => {
+        mensajeDiv.textContent = '';
+    }, 3000); // 3000 milisegundos = 3 segundos
 }
