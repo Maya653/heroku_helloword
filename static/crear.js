e556b501d1a31d41f8cf93a8e11003ed55a7027f
Maya653 <122810922+Maya653@users.noreply.github.com>
2023-12-03 03:42:35 +0000

    update mensajes en crear y eliminar

--- static/crear.js
+++ static/crear.js
@@ -4,6 +4,8 @@ async function crearContacto() {
         const nombre = prompt("Ingrese el nombre del nuevo contacto:");
         const telefono = prompt("Ingrese el teléfono del nuevo contacto:");
 
+        const contactosDiv = document.getElementById('resultadoBusqueda');
+
         const response = await fetch('https://contactos-backen-b4d88f351253.herokuapp.com/contactos', {
             method: "POST",
             headers: {
@@ -14,6 +16,15 @@ async function crearContacto() {
 
         const data = await response.json();
         console.log(data);
+
+        // Mostrar el resultado en el elemento con id 'resultadoBusqueda'
+        contactosDiv.innerHTML = `
+            <p>Resultado de la creación:</p>
+            <p>ID: ${data.id}</p>
+            <p>Email: ${data.email}</p>
+            <p>Nombre: ${data.nombre}</p>
+            <p>Teléfono: ${data.telefono}</p>
+        `;
     } catch (error) {
         console.error('Error:', error);
     }
--- static/eliminar.js
+++ static/eliminar.js
@@ -1,6 +1,13 @@
+// Función para mostrar mensajes en la interfaz de usuario
+function mostrarMensaje(mensaje) {
+    alert(mensaje); // Puedes personalizar esto según tu interfaz
+}
+
+// Función asincrónica para eliminar un contacto
 async function eliminarContacto() {
     try {
         const email = document.getElementById('eliminarEmail').value;
+        const contactosDiv = document.getElementById('resultadoBusqueda');
 
         const response = await fetch(`https://contactos-backen-b4d88f351253.herokuapp.com/contactos/${email}`, {
             method: "DELETE",
@@ -9,6 +16,9 @@ async function eliminarContacto() {
         if (response.status === 204) {
             // Éxito al eliminar el contacto
             mostrarMensaje('Contacto eliminado exitosamente.');
+
+            // Limpiar el contenido del elemento con id 'resultadoBusqueda'
+            contactosDiv.innerHTML = '';
         } else {
             // Error al intentar eliminar el contacto
             mostrarMensaje('Error al intentar eliminar el contacto.');
@@ -18,13 +28,3 @@ async function eliminarContacto() {
     }
 }
 
-function mostrarMensaje(mensaje) {
-    // Mostrar el mensaje en un div con el id "mensajeEliminacion"
-    const mensajeDiv = document.getElementById('mensajeEliminacion');
-    mensajeDiv.textContent = mensaje;
-
-    // Limpiar el mensaje después de unos segundos (opcional)
-    setTimeout(() => {
-        mensajeDiv.textContent = '';
-    }, 3000); // 3000 milisegundos = 3 segundos
-}
