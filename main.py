ea67be00198097f121fc51e61182160669da2727
Maya653 <122810922+Maya653@users.noreply.github.com>
2023-12-03 01:00:48 +0000

    update

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 @app.route("/",  methods=["GET"])
 def index():
     return render_template('index.html')
+
 @app.route("/listar", methods=["GET"])
 def listar():
     # Lógica para listar contactos
