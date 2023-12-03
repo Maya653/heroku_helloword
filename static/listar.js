4fc25c3e625069e64e249b1d9744894debf184b8
Maya653 <122810922+Maya653@users.noreply.github.com>
2023-12-03 01:37:43 +0000

    update

--- static/listar.js
+++ static/listar.js
@@ -1,23 +1,34 @@
+// Define la URL del API
 const apiUrl = "https://contactos-backen-b4d88f351253.herokuapp.com/contactos";
 
+// Función asincrónica para listar contactos
 async function listarContactos() {
     try {
-        const response = await fetch(apiUrl, {method: 'GET', mode: 'cors'});
+        // Realiza una solicitud GET al API usando fetch
+        const response = await fetch(apiUrl, { method: 'GET', mode: 'cors' });
+
+        // Parsea la respuesta como JSON
         const data = await response.json();
 
+        // Obtiene el elemento con el id 'resultadoBusqueda'
         const contactosDiv = document.getElementById('resultadoBusqueda');
+
+        // Construye la lista de contactos en el elemento HTML
         contactosDiv.innerHTML = '<h2>Listado de contactos:</h2><ul>';
 
         if (data.length > 0) {
+            // Si hay contactos, los muestra en la lista
             data.forEach(contacto => {
                 contactosDiv.innerHTML += `<li>${contacto.nombre} - ${contacto.email} - ${contacto.telefono}</li>`;
             });
         } else {
+            // Si no hay contactos, muestra un mensaje
             contactosDiv.innerHTML += '<p>No hay contactos para mostrar.</p>';
         }
 
         contactosDiv.innerHTML += '</ul>';
     } catch (error) {
+        // Captura y muestra cualquier error en la consola
         console.error('Error:', error);
     }
 }
