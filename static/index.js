0444c63e0cb297ed6b32dcf8144833b9146de927
Maya653 <122810922+Maya653@users.noreply.github.com>
2023-12-03 00:04:08 +0000

    update

---
+++ static/index.js
@@ -0,0 +1,11 @@
+// static/MainComponent.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MainComponent from './static/MainComponent'; // Ajusta según la estructura de tu proyecto
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MainComponent />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
