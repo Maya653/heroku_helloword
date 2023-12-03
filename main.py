6496caa0611973e4830bd2e031c08c77b57d6eb5
Maya653 <122810922+Maya653@users.noreply.github.com>
2023-12-03 00:45:05 +0000

    update

--- main.py
+++ main.py
@@ -3,17 +3,29 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/",  methods=["GET"])
-def index():
-    return render_template('index.html')
+@app.route("/listar", methods=["GET"])
+def listar():
+    # Lógica para listar contactos
+    return render_template('listar.html')
 
-@app.route("/ver", methods=["GET", "POST"])
-def ver():
-    return render_template('ver.html')
+@app.route("/actualizar", methods=["GET"])
+def actualizar():
+    # Lógica para actualizar contactos
+    return render_template('actualizar.html')
 
-@app.route("/observar", methods=["GET"])
-def observar():
-    return render_template('observar.html')
+@app.route("/eliminar", methods=["GET"])
+def eliminar():
+    # Lógica para eliminar contactos
+    return render_template('eliminar.html')
 
+@app.route("/buscar", methods=["GET"])
+def buscar():
+    # Lógica para buscar contactos
+    return render_template('buscar.html')
+
+@app.route("/crear", methods=["GET"])
+def crear():
+    # Lógica para crear contactos
+    return render_template('crear.html')
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
