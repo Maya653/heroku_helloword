d7131cc0d32ffb48dd19ae0ba330b67b3d7ad672
Maya653 <122810922+Maya653@users.noreply.github.com>
2023-12-03 00:38:16 +0000

    update

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os 
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -15,6 +16,4 @@ def observar():
     return render_template('observar.html')
 
 if __name__ == '__main__':
-    # Puerto 5000 es el predeterminado para Flask, pero Heroku asigna un puerto diferente
-    # Ajusta el host y el puerto para que funcione tanto localmente como en Heroku
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
